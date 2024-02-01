53601e24c20f3aac12d3f1f178a61e4036ec5bd8
MilanSad <milansadilek@post.cz>
2024-02-02 00:31:49 +0100

    Konečná verze

---
+++ Ukoly/Module 6. Tuples, Sets, Dictionaries.Part 2.py
@@ -0,0 +1,26 @@
+"""
+Task 2
+Create an app English-French Dictionary. Store a word
+in English and its French translation. Provide the possibility
+to add, delete, search, and replace data. Use a dictionary to
+store information.
+Task 3
+Create an app Company. Store the following information
+about a person: full name, phone number, corporate email,
+job title, room number, skype. Provide the possibility to add,
+delete, search, and replace data. Use a dictionary to store
+information.
+Task 4
+Create an app Book Collection. Store the following information
+about books: author, title, genre, year of release,
+publisher. Provide the possibility to add, delete, search, and
+replace data. Use a dictionary to store information.
+"""
+
+"""
+Task 1
+Create an app that stores information about great basketball
+players. Store the player’s full name and height. Provide
+the possibility to add, delete, search, and replace data. Use
+a dictionary to store information.
+"""
